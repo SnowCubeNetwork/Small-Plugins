b29019cfa29cdc9dcda255252293b4c1cca7c16c
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-12-16 19:40:34 -0800

    Update PriorityPrefixEnum.php

--- ChatChannels/src/chatchannels/PriorityPrefixEnum.php
+++ ChatChannels/src/chatchannels/PriorityPrefixEnum.php
@@ -4,35 +4,41 @@ namespace chatchannels;
 
 use pocketmine\plugin\Plugin;
 
-class PriorityPrefixEnum{
-	/** @var array[] */
-	private $registries = [];
+class PriorityPrefixEnum
+{
 
-	public function add(Prefix $prefix, Plugin $context){
-		$this->registries[] = ["prefix" => $prefix, "plugin" => $context];
-	}
+    /** @var array[] */
+    private $registries = [];
 
-	public function recalculate(){
-		foreach($this->registries as $k => $reg){
-			/** @var Plugin $plugin */
-			$plugin = $reg["plugin"];
-			if(!$plugin->isEnabled()){
-				unset($this->registries[$k]);
-			}
-		}
-		$this->registries = array_values($this->registries);
-	}
+    public function add(Prefix $prefix, Plugin $context)
+    {
+        $this->registries[] = ["prefix" => $prefix, "plugin" => $context];
+    }
 
-	public function getPrefixes(ChannelSubscriber $sender, Channel $channel, $recalculate = false){
-		if($recalculate){
-			$this->recalculate();
-		}
-		$output = "";
-		foreach($this->registries as $reg){
-			/** @var Prefix $prefix */
-			$prefix = $reg["prefix"];
-			$output .= $prefix->getPrefix($sender, $channel);
-		}
-		return $output;
-	}
+    public function recalculate()
+    {
+        foreach ($this->registries as $k => $reg) {
+            /** @var Plugin $plugin */
+            $plugin = $reg["plugin"];
+            if (!$plugin->isEnabled()) {
+                unset($this->registries[$k]);
+            }
+        }
+        $this->registries = array_values($this->registries);
+    }
+
+    public function getPrefixes(ChannelSubscriber $sender, Channel $channel, $recalculate = false)
+    {
+        if ($recalculate) {
+            $this->recalculate();
+        }
+        $output = "";
+        foreach ($this->registries as $reg) {
+            /** @var Prefix $prefix */
+            $prefix = $reg["prefix"];
+            $output .= $prefix->getPrefix($sender, $channel);
+        }
+        return $output;
+    }
+
 }
