5c1506302ce3e69bb3be0af9a488629794000a63
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-12-16 19:41:11 -0800

    Update SessionControl.php

--- ChatChannels/src/chatchannels/SessionControl.php
+++ ChatChannels/src/chatchannels/SessionControl.php
@@ -9,52 +9,62 @@ use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\event\plugin\PluginDisableEvent;
 use pocketmine\Player;
 
-class SessionControl implements Listener{
-	private $plugin;
-	/** @var PlayerSubscriber[] */
-	private $playerSubs = [];
+class SessionControl implements Listener
+{
 
-	public function __construct(ChatChannels $plugin){
-		$this->plugin = $plugin;
-	}
+    private $plugin;
+    /** @var PlayerSubscriber[] */
+    private $playerSubs = [];
 
-	public function onPluginDisabled(PluginDisableEvent $e){
-		$this->plugin->getPrefixAPI()->recalculateAll($e->getPlugin());
-	}
+    public function __construct(ChatChannels $plugin)
+    {
+        $this->plugin = $plugin;
+    }
 
-	public function onJoin(PlayerJoinEvent $event){
-		$player = $event->getPlayer();
-		$this->playerSubs[$player->getId()] = new PlayerSubscriber($this->plugin, $player);
-	}
+    public function onPluginDisabled(PluginDisableEvent $e)
+    {
+        $this->plugin->getPrefixAPI()->recalculateAll($e->getPlugin());
+    }
 
-	/**
-	 * @param Player $player
-	 *
-	 * @return PlayerSubscriber
-	 */
-	public function getSession(Player $player){
-		return $this->playerSubs[$player->getId()];
-	}
+    public function onJoin(PlayerJoinEvent $event)
+    {
+        $player = $event->getPlayer();
+        $this->playerSubs[$player->getId()] = new PlayerSubscriber($this->plugin, $player);
+    }
 
-	public function onQuit(PlayerQuitEvent $event){
-		$p = $event->getPlayer();
-		if(isset($this->playerSubs[$p->getId()])){
-			$this->playerSubs[$p->getId()]->release();
-			unset($this->playerSubs[$p->getId()]);
-		}
-	}
+    /**
+     * @param Player $player
+     *
+     * @return PlayerSubscriber
+     */
+    public function getSession(Player $player)
+    {
+        return $this->playerSubs[$player->getId()];
+    }
 
-	public function onChat(PlayerChatEvent $event){
-		$event->setCancelled();
-		$player = $event->getPlayer();
-		$sub = $this->playerSubs[$player->getID()];
-		$sub->onChatEvent($event->getMessage());
-	}
+    public function onQuit(PlayerQuitEvent $event)
+    {
+        $p = $event->getPlayer();
+        if(isset($this->playerSubs[$p->getId()])){
+            $this->playerSubs[$p->getId()]->release();
+            unset($this->playerSubs[$p->getId()]);
+        }
+    }
 
-	public function setMute(Player $player, $muted){
-		$sub = $this->playerSubs[$player->getId()];
-		$original = $sub->muted;
-		$sub->muted = $muted;
-		return $original;
-	}
+    public function onChat(PlayerChatEvent $event)
+    {
+        $event->setCancelled();
+        $player = $event->getPlayer();
+        $sub = $this->playerSubs[$player->getID()];
+        $sub->onChatEvent($event->getMessage());
+    }
+
+    public function setMute(Player $player, $muted)
+    {
+        $sub = $this->playerSubs[$player->getId()];
+        $original = $sub->muted;
+        $sub->muted = $muted;
+        return $original;
+    }
+
 }
